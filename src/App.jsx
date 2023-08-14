102a160fd56bcc8c933b0414198fd059e29f8cd9
sahil <sahil242bhatiya@gmail.com>
2023-08-14 09:56:12 +0530

    Optimized UI and updated styles across the application
    This commit implements several improvements in the UI of the application for better user experience. The existing layout in App.jsx was shifted to a more responsive grid structure. Themes were adjusted in themes/mytheme/theme.scss. Base styles in _base.scss were updated and some rules such as resetting margin, padding and box-sizing were added. The import of unnecessary external stylesheets, specifically the "primereact.min.css" in main.jsx, was also removed. A minor clean-up was done in the dependencies section of package.json as Firebase was no longer needed. The style variables in _variables.scss were rearranged for better readability and consistency. Commit includes updates towards the goal of maintaining a clean, optimal and consistent codebase.

--- src/App.jsx
+++ src/App.jsx
@@ -8,32 +8,44 @@ function App() {
   const [checked, setChecked] = useState(false);
   return (
     <>
-      <div className="d-flex align-items-center w-100">
-        <Button type="button" className="h-40 mx-3" rounded>
-          Tražim
-        </Button>
-        <Button
-          type="button"
-          severity="secondary"
-          className="h-40 mx-3"
-          rounded
-        >
-          Tražim
-        </Button>
-        <AutoComplete className="rounded-4"></AutoComplete>
-        <InputSwitch
-          className="mx-3"
-          checked={checked}
-          onChange={(e) => setChecked(e.value)}
-        />
-        <TabView>
-          <TabPanel header="Tražim">
-            <h1>Content I</h1>
-          </TabPanel>
-          <TabPanel header="Nudim">
-            <h1>Content II</h1>
-          </TabPanel>
-        </TabView>
+      <div className="container p-0">
+        <div className="row align-items-center w-100 m-0">
+          <div className="col-md-2">
+            <Button type="button" className="h-40 mx-3" rounded>
+              Tražim
+            </Button>
+          </div>
+          <div className="col-md-2">
+            <Button
+              type="button"
+              severity="secondary"
+              className="h-40 mx-3"
+              rounded
+            >
+              Tražim
+            </Button>
+          </div>
+          <div className="col-md-2">
+            <AutoComplete className="rounded-4"></AutoComplete>
+          </div>
+          <div className="col-md-2">
+            <InputSwitch
+              className="mx-3"
+              checked={checked}
+              onChange={(e) => setChecked(e.value)}
+            />
+          </div>
+          <div className="col-md-2">
+            <TabView>
+              <TabPanel header="Tražim">
+                <h1>Content I</h1>
+              </TabPanel>
+              <TabPanel header="Nudim">
+                <h1>Content II</h1>
+              </TabPanel>
+            </TabView>
+          </div>
+        </div>
       </div>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "primereact/resources/primereact.min.css";
 import "/src/core/styles/themes/mytheme/theme.scss";
 import "/src/core/styles/_base.scss";
 import "/src/core/styles/_transitions.scss";
