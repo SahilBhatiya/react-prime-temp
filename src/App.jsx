097fb4fee81d16921fd19aea6d662127425e968b
sahil <sahil242bhatiya@gmail.com>
2023-08-15 19:06:11 +0530

    Refactor App.jsx and update styles for improved UI
    Updated `App.jsx` to improve the user interface and application performance. Removed `TabView` and `TabPanel`, replacing them with `Card` and `Carousel`. Switched from `animateElement` to `animateElementWithAllChildren` to ensure all child elements are correctly animated. Created a new method to change page display in the carousel with `changePage` and `resetPage`. These changes make the application more user friendly and efficient.
    
    Additionally, the styling files have been updated to support these changes. New classes `.w-180`, `.h-320` and `.home__screen__cards` have been added in `src/core/styles/_base.scss` to support the `Carousel` component in `App.jsx`.
    
    Changed the visibility of `p-carousel-item` from hidden to visible in `.p-items-hidden` css rule inside `src/core/styles/theme-base/_main-base.css` to ensure carousel items are visible when they need to be.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
 import { Button } from "primereact/button";
 import { useEffect, useRef, useState } from "react";
-import { TabPanel, TabView } from "primereact/tabview";
-import {
-  animateElement,
-  animateElementWithAllChildren,
-} from "./helpers/animate-element/animate-element.helper.js";
+import { animateElementWithAllChildren } from "./helpers/animate-element/animate-element.helper.js";
+
+import { Card } from "primereact/card";
+import { Carousel } from "primereact/carousel";
 
 function App() {
   const FADE_DURATION = 300;
   const SCALE_DURATION = 120;
-  const [checked, setChecked] = useState(false);
   const parentRef = useRef(null);
   const dropdownRef = useRef(null);
   const LENGTH = 10;
   const [values, setValues] = useState([]);
+  const [page, setPage] = useState(0);
   useEffect(() => {
-    animateElement(parentRef, "scale", SCALE_DURATION);
+    animateElementWithAllChildren(parentRef, "scale", SCALE_DURATION);
   }, [parentRef]);
   useEffect(() => {
     animateElementWithAllChildren(dropdownRef, "fade", FADE_DURATION);
@@ -42,30 +41,45 @@ function App() {
     });
   };
 
+  const changePage = () => {
+    setPage((prev) => prev + 1);
+  };
+
+  const resetPage = () => {
+    setPage(0);
+  };
+
+  const cardTemplate = (value) => {
+    return <Card className="w-180 h-320">{value}</Card>;
+  };
+
   return (
     <>
       <div className="container p-4">
-        <div className="row gap-2 justify-content-start" ref={parentRef}>
-          {values.map((value, index) => {
-            return (
-              <div className="col-5 col-md-1 p-3" key={index}>
-                {value}
-              </div>
-            );
-          })}
+        <div className="row" ref={parentRef}>
+          <Carousel
+            value={values}
+            numScroll={1}
+            page={page}
+            numVisible={7}
+            itemTemplate={cardTemplate}
+            showIndicators={false}
+            showNavigators={true}
+            containerClassName="home__screen__cards"
+          />
         </div>
         <div className="row align-items-center w-100 m-0">
           <div className="col-md-4 fixed-bottom mb-2">
             <Button
-              onClick={() => generateValues(5)}
+              onClick={() => changePage()}
               type="button"
               className="h-40 mx-3 "
               rounded
             >
-              Add
+              Next
             </Button>
             <Button
-              onClick={resetValues}
+              onClick={resetPage}
               type="button"
               severity="secondary"
               className="h-40 mx-3"
@@ -74,24 +88,24 @@ function App() {
               Reset
             </Button>
           </div>
-          <div
-            className="col-md-2 position-fixed top-10 p-2 right-10 shadow-lg rounded-4 bg-white transition-all-auto min-width min-h-200"
-            ref={dropdownRef}
-          >
-            <TabView>
-              <TabPanel header="Tražim">
-                <div className="d-flex flex-column justify-content-center align-items-center">
-                  <h1>Content I</h1>
-                </div>
-              </TabPanel>
-              <TabPanel header="Nudim">
-                <div className="d-flex flex-column justify-content-center align-items-center">
-                  <h1>Content II</h1>
-                  <div>asdasd</div>
-                </div>
-              </TabPanel>
-            </TabView>
-          </div>
+          {/*<div*/}
+          {/*  className="col-md-2 p-2  shadow-lg rounded-4 bg-white transition-all-auto min-width min-h-200"*/}
+          {/*  ref={dropdownRef}*/}
+          {/*>*/}
+          {/*  <TabView>*/}
+          {/*    <TabPanel header="Tražim">*/}
+          {/*      <div className="d-flex flex-column justify-content-center align-items-center">*/}
+          {/*        <h1>Content I</h1>*/}
+          {/*      </div>*/}
+          {/*    </TabPanel>*/}
+          {/*    <TabPanel header="Nudim">*/}
+          {/*      <div className="d-flex flex-column justify-content-center align-items-center">*/}
+          {/*        <h1>Content II</h1>*/}
+          {/*        <div>asdasd</div>*/}
+          {/*      </div>*/}
+          {/*    </TabPanel>*/}
+          {/*  </TabView>*/}
+          {/*</div>*/}
         </div>
       </div>
     </>
